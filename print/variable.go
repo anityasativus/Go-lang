ff03dc0364586e1150c6fc82b370057297d74554
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-01-13 12:27:58 +0600

    variable update

--- print/variable.go
+++ print/variable.go
@@ -10,11 +10,21 @@ func main(){
 	fmt.Println(a)
 
 	*/
+
 	//variable// 
-	
 var an int =19
+Z:=20.32
 
 fmt.Print(an)
-	
+fmt.Print(Z)
+
+
+//string//
+c:="hello world"
+fmt.Println(c)
+
+//bool//
+a:=true
+fmt.Println(a)	
 
 }
