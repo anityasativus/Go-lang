1b2771ba3222b69d5d9913eabae3d6990fc088cb
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-01-29 18:53:23 +0600

    add

--- print/Functionwithreturnvalue.go
+++ print/Functionwithreturnvalue.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
-func add(num1 int ,num2 int )  {
-	sum:=num1+num2
-	fmt.Println(sum)
-}
+func add(num1 int ,num2 int ) int { //num=10,num2=20
 
+	sum := num1+num2
+
+	return sum 
+}
+func getnumbers(num1 int ,num2 int)(int , int){
+	sum := num1+num2
+}
 func main(){
 
 	a:=10
 	b:=20
 
-	add(a,b)
-	add(3,3)
+	sum := add(a,b)
+
+	fmt.Println(sum)
+
 }
