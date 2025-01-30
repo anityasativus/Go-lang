ec85fa3fb2adb98c643a6b40999d75f619f62842
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-01-30 12:06:18 +0600

    new Example

---
+++ print/morefuncexample.go
@@ -0,0 +1,33 @@
+package main 
+
+import "fmt"
+
+func add(num1 int ,num2 int ) int { //num=10,num2=20
+
+	sum := num1+num2
+
+	return sum 
+}
+
+func getnumbers(num1 int ,num2 int)(int , int){
+	
+	sum := num1+num2//ans30
+	
+	mul:=num1*num2//ans200
+	
+	return sum,mul 
+}
+
+func printSomething(){
+	fmt.Println("Education must be free!")
+	
+}
+func sayHello(name string){
+	fmt.Println("Welcome")
+}
+func main(){
+
+	printSomething()
+	sayHello("Anitya")
+	
+}
