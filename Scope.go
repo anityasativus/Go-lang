ba05c3e433c41c85cdc82e4306f99185502d9c56
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-01-31 10:32:56 +0600

    more scrope

---
+++ Scope.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var a = 20
+var b = 30
+
+func add(x int, y int) {
+	z := x + y
+	fmt.Println(z)
+}
+func main() {
+	var p = 30
+	var q = 40
+	add(p,q)
+
+	add(a,b)
+	
+	add(a,p)
+}
