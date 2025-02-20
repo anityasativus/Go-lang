3cc98cdc0e03d935b31823c2c47ce90dc177cd16
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-02-21 00:00:02 +0600

    up

--- Local_scopeNBlock.go
+++ Local_scopeNBlock.go
@@ -1,22 +1,26 @@
 package main
 
+import "fmt"
 
+var (
+	a = 20
+	b = 30
+)
 
-var(
-	 a = 20
-      b = 30
-) 
 /*
 
 
-*/
- 
+ */
+
 func main() {
 
-x:=18
+	x := 18
 
-if x>=18{
+	if x >= 18 {
+		p:=10
+		fmt.Println("i am matured boy")
+		fmt.Println("i have" ,p,"gff")
 
-}
+	}
 
 }
