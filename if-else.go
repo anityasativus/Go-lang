a15a3541e3b860b9aa4876df783d322c18e59e1b
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-03-11 00:19:26 +0600

    f

--- if-else.go
+++ if-else.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	age :=2
-	if age > 18 {
+	if age>18 {
         fmt.Println("You are eligible to be married.")
     } else if age<18 {
         fmt.Println("You are not eligible to be married, but you can love someone.")
