a6e58a589551b5c9310f8512d6f4f668b9e7e0ed
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-03-13 22:19:04 +0600

    s

--- if-else.go
+++ if-else.go
@@ -9,6 +9,7 @@ func main() {
         fmt.Println("You are eligible to be married.")
     }
      else if age<18 {
+        
         fmt.Println("You are not eligible to be married, but you can love someone.")
     } else {
         fmt.Println("You are 18 years old,which is often considered the age of  eligibility for marriage.")
